2be60abef740bccf0692d300a9feabfa4cb6391a
ZeWaka <zewakagamer@gmail.com>
2021-03-06 21:30:54 -0800

    Update src/app/cmd/remove_metadata.cpp

--- src/app/cmd/remove_metadata.cpp
+++ src/app/cmd/remove_metadata.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2015  David Capello
+// Copyright (C) 2021 Igara Studio S.A.
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
